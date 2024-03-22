78b8ad5a722f8555cf185cf252a693e1bc44f020
Alberto Cangialosi <billyballo.res@gmail.com>
2024-03-22 12:11:15 +0100

    fix: clear comments in loginScreen.js

--- src/app/component/loginScreen.js
+++ src/app/component/loginScreen.js
@@ -6,11 +6,8 @@ export const LoginScreen = () => {
   function loginToGitHub() {
     const clientID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
     const scope = "user:email";
-    // NOTE : it's best to use `encodeURIComponent` if I want to use it inside of URL query parameters, for a full URL without query parameters `encodeURI` should suffice
-    const redirectUri = encodeURIComponent("http://localhost:3000/api/callback");
 
-
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=${scope}&redirect_uri=${redirectUri}`;
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=${scope}`;
   }
 
   return (
